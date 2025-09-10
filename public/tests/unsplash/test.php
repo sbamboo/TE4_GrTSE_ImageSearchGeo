45343e16b2d5da87fdeae563aefc4f624eef9fbc
FiggleBottomFB <elias.moll38@gmail.com>
2025-09-10 10:17:05 +0200

    statred input in index.php

--- public/tests/unsplash/test.php
+++ public/tests/unsplash/test.php
@@ -4,7 +4,7 @@ require_once('./../../php/unsplash_api.php');
 $SECRETS = parse_ini_file(__DIR__ . '/../../../php_secrets.ini', false, INI_SCANNER_TYPED); // Replace with your Unsplash API Access Key
 
 
-$unsplash = new UnsplashAPI($SECRETS['unsplash_access_key']);
+$unsplash = new UnsplashAPI($SECRETS['UNSPLASH_ACCESS_KEY']);
 
 try {
     $random_photo = $unsplash->make_get_request('photos/random');
