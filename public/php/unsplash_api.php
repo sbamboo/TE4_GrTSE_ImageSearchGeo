759961481b246cd195acc664394801ad72e67780
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-09-10 10:05:48 +0200

    Update unsplash_api.php

--- public/php/unsplash_api.php
+++ public/php/unsplash_api.php
@@ -24,25 +24,6 @@ class UnsplashAPI {
         }
         return json_decode($response, true);
     }
-
-    // lower level make POST request function without curl extension
-    function make_post_request($endpoint, $data = []) {
-        $url = $this->api_url . $endpoint . '?client_id=' . $this->access_key;
-        $options = [
-            'http' => [
-                'method' => 'POST',
-                'header' => "Content-Type: application/json\r\n" .
-                            "Accept: application/json\r\n",
-                'content' => json_encode($data)
-            ]
-        ];
-        $context = stream_context_create($options);
-        $response = file_get_contents($url, false, $context);
-        if ($response === FALSE) {
-            throw new Exception("Error making POST request to Unsplash API");
-        }
-        return json_decode($response, true);
-    }
 }
 
 /*
@@ -51,9 +32,7 @@ $unsplash = new UnsplashAPI($SECRETS['unsplash_access_key']);
 
 try {
     $random_photo = $unsplash->make_get_request('photos/random');
-    echo '<pre>';
     print_r($random_photo);
-    echo '</pre>';
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage();
 }
