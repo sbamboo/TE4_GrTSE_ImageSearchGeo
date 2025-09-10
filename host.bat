76c919b2ad0d4f99eccb672be11a40c4c2d70078
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-09-10 13:02:09 +0200

    Update host.bat

--- host.bat
+++ host.bat
@@ -1,4 +1,14 @@
 @echo off
+REM Bypass Ctrl+C Prompt
+if "%~1"=="-FIXED_CTRL_C" (
+   REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+) ELSE (
+   REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :EOF
+)
+
 REM Save current working directory
 set "OLD_DIR=%cd%"
 
