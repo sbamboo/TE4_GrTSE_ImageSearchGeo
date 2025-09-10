5fb80a97a236dc151fe2f631dd0d014b8ba9399a
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-09-10 08:56:39 +0200

    Add root project setup

--- host.bat
+++ host.bat
@@ -1,1 +1,12 @@
+@echo off
+REM Save current working directory
+set "OLD_DIR=%cd%"
+
+REM Change to public directory
+cd /d "%~dp0public"
+
+REM Start PHP server (adjust command as needed)
 php -S localhost:8080
+
+REM After PHP exits, change back
+cd /d "%OLD_DIR%"
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+// Get all the secrets from php.ini file
+$SECRETS = parse_ini_file(__DIR__ . '/../php.ini', false, INI_SCANNER_TYPED);
+
+
+?>
