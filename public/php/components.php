2554376960205a3f81cf8ffd6970380d202e35c0
FiggleBottomFB <elias.moll38@gmail.com>
2025-09-11 11:38:49 +0200

    return select values function

--- public/php/components.php
+++ public/php/components.php
@@ -22,5 +22,14 @@ function echoProgImg($blurrySrc, $fullSrc, $alt = "", $id="") {
 
 // Return <select> with <option>s inside for the filter options
 
+function echoFilter($name, array $options, $selected = null){
+    $html = "<select name=\"$name\" id=\$name\">\n";
+    foreach($options as $value => $label){
+        $isSelected = ($value == $selected) ? " selected" : "";
+        $html .= "<option value=\"$value\"$isSelected>$label</option>\n";
+    }
+    $html .= "</select>\n";
+    return $html;
+}
 
 ?>
