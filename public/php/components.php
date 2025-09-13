2f8c9aca069bde679f513c73da1cadc6491a2d6c
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-09-13 14:08:08 +0200

    corner-round images

--- public/php/components.php
+++ public/php/components.php
@@ -30,7 +30,7 @@ function echoImageDownloadBadge(string $slug, string $url): void {
     $html = <<<EOF
 <a class="image-photo-download grid-item-download-badge" 
    href="{$proxyUrl}" target="_blank">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
          viewBox="0 0 24 24" fill="none" stroke="currentColor" 
          stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
