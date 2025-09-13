6ff7747b58674be440b5f79f273fd74bcd2be026
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-09-13 14:24:28 +0200

    Update popup stub

--- public/js/popups.js
+++ public/js/popups.js
@@ -1,5 +1,7 @@
 // File containig reusable code for popups
 
+window.hasAssignedBodyClickListener = false; // Global variable to track if body click listener is assigned
+
 class Popups {
     constructor() {
         // If DOM has not loaded schedule constructor to run on DOMContentLoaded
@@ -15,6 +17,39 @@ class Popups {
 
         this.shownPopups = []; // Stores elements
         this.shownPortals = []; // Stores elements
+
+        // Global click outside listener by us assinging a listener to body if one is not already asigned
+        if (!window.hasAssignedBodyClickListener) {
+            document.body.addEventListener('click', (event) => {
+                // Get all elements where we clicked using elementsFromPoint
+                const elementsAtClick = document.elementsFromPoint(event.clientX, event.clientY);
+                const clickedPopup = this.shownPopups.find(popup => elementsAtClick.includes(popup));
+                const clickedPortal = this.shownPortals.find(portal => elementsAtClick.includes(portal));
+
+                // If we clicked outside any shown popup or portal, hide all popups and portals that are set to close on click outside
+                if (!clickedPopup) {
+                    this.shownPopups = this.shownPopups.filter(popup => {
+                        if (popup.dataset.closeOnClickOutside === 'true') {
+                            popup.style.display = 'none';
+                            return false; // Remove from shownPopups
+                        }
+                        return true; // Keep in shownPopups
+                    });
+                }
+
+                if (!clickedPortal) {
+                    this.shownPortals = this.shownPortals.filter(portal => {
+                        if (portal.dataset.closeOnClickOutside === 'true') {
+                            portal.style.display = 'none';
+                            return false; // Remove from shownPortals
+                        }
+                        return true; // Keep in shownPortals
+                    });
+                }
+            });
+
+            window.hasAssignedBodyClickListener = true; // Mark listener as assigned
+        }
     }
 
     // Show a overlay popup (A popup that is above all content and centered on the screen)
